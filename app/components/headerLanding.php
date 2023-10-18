49fbf71750c422eed2eae73f4889a383f951be78
LukmanHafidzzz <lukmanhafidz30@gmail.com>
2023-10-18 20:14:44 +0700

    fourth commit

--- app/components/headerLanding.php
+++ app/components/headerLanding.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
 
 <body>
@@ -17,17 +18,10 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-                <!-- <div class="me-4">
-                    <a class="nav-link" aria-current="page" href="#home">Home</a>
-                </div>
-                <div class="me-4">
-                    <a class="nav-link" aria-current="page" href="#about">About</a>
-                </div>
-                <div class="me-4">
-                    <a class="nav-link" aria-current="page" href="#profile">Profile</a>
-                </div> -->
                 <div>
-                    <a class="nav-link" aria-current="page" href="registerOption.php">Login</a>
+                    <a class="nav-link" aria-current="page" href="registerOption.html">
+                        <button class="btn loginBtn" type="submit">LOGIN</button>
+                    </a>
                 </div>
             </div>
         </div>
